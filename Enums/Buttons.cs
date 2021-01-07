b1014cb7008a5898de8a94d6b0b0c76feba5e141
MummiTroll <56731370+MummiTroll@users.noreply.github.com>
2021-01-07 22:22:47 +0100

    Add files via upload

---
+++ Enums/Buttons.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelBook.Enums
+{
+    public enum Buttons
+    {
+        ImportDBFile = 0,
+        Source1Del = 1,
+        Source2Del = 2,
+        Source3Del = 3,
+        Target1Del = 4,
+        Target2Del = 5,
+        Target3Del = 6,
+        Drives1Del = 7,
+        Drives2Del = 8,
+        Drives3Del = 9,
+        DrivesExcludedDel1 = 10,
+        DrivesExcludedDel2 = 11,
+        DrivesExcludedDel3 = 12
+    }
+}
